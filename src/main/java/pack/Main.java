659ea2487adb036f410cd6d71827392c7e348612
yevheniia <suleimanovayev@gmail.com>
2021-10-31 21:00:15 +0200

    com inuit

---
+++ src/main/java/pack/Main.java
@@ -0,0 +1,12 @@
+package pack;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Main {
+    private static final String A = "hjfsdhjfsdjh";
+
+    public String tryuioio() {
+        return A;
+    }
+}
