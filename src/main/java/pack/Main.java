e1cb8e1940975c894d75fb864b115728983493dc
suleimanovayev <suleimanovayev@gmail.com>
2021-11-07 16:51:04 +0200

    from laptop

--- src/main/java/pack/Main.java
+++ src/main/java/pack/Main.java
@@ -4,7 +4,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class Main {
-    private static final String A = "hjfsdhjfsdjh";
+    private static final String A = "From Laptop";
 
     public String tryuioio() {
         return A;
